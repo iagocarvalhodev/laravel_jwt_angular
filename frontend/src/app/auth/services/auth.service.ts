d46e7045133eddd960d81e763c467887d73f921f
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-24 16:52:44 -0200

    implementando metodo logout

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,13 +1,15 @@
+import { User } from './../interfaces/user';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.prod';
 import 'rxjs/add/operator/do';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-constructor(private http: HttpClient) { }
+constructor(private http: HttpClient, private router: Router) { }
 
 check(): boolean {
   return localStorage.getItem('user') ? true : false;
@@ -19,6 +21,31 @@ check(): boolean {
         localStorage.setItem('token', data.token);
         localStorage.setItem('user', btoa(JSON.stringify(data.user)));
       });
+  }
+
+  logout(): void {
+    this.http.get(`${environment.api_url}/auth/logout`).subscribe(resp => {
+      console.log(resp);
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      this.router.navigate(['auth/login']);
+    });
+  }
+
+
+  getUser(): User {
+    return localStorage.getItem('user') ? JSON.parse(atob(localStorage.getItem('user'))) : null;
+  }
+
+  setUser(): Promise <boolean> {
+    return this.http.get<any>(`${environment.api_url}/auth/me`).toPromise()
+      .then(data => {
+        if(data.user){
+          localStorage.setItem('user', btoa(JSON.stringify(data.user)));
+          return true;
+        }
+        return false;
+      })
   }
 
 
